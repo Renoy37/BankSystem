65ab822132283bd4dbf55860c514f9c99bd91211
Renoy37 <robinmgk077@gmail.com>
2024-04-16 10:21:22 +0300

    Completed pipfile set up

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# from flask import Flask, request, make_response
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db
