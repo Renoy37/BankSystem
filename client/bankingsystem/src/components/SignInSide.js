836426f32b95f0e9a09092fa6c8a1f29f7ce9596
Robins Mugambi <119851674+Renoy37@users.noreply.github.com>
2024-06-03 14:29:21 +0300

    Updated SignInSide.js

--- client/bankingsystem/src/components/SignInSide.js
+++ client/bankingsystem/src/components/SignInSide.js
@@ -46,7 +46,7 @@ function SignInSide({ onLogin, onSignUp }) {
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('http://127.0.0.1:8000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
